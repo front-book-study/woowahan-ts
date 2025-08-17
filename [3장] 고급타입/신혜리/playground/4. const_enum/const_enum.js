03cd61113e28ab198672e61e22c29b71ca27c21a
rachel490 <ccori12@naver.com>
2025-08-17 20:47:40 +0900

    플레이그라운드 삭제

--- [3장] 고급타입/신혜리/playground/4. const_enum/const_enum.js
+++
@@ -1,7 +0,0 @@
-// const enum NumberEnum {
-//   ONE = 1,
-//   TWO = 2,
-// }
-const myStringNumber = "ONE" /* STRING_NUMBER['ONE'] */;
-console.log("ONE" /* STRING_NUMBER['ONE'] */);
-// console.log(STRING_NUMBER[0]);
